0df99318b474624694de26ca2ac13fabddfed3ff
michaelrychly <MichaelRychly@gmail.com>
2018-03-20 14:25:25 -0400

    show urls and shortened urls

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -9,6 +11,27 @@ var urlDatabase = {
 
 app.get("/", (req, res) => {
   res.end("Hello!");
+});
+
+app.get("/urls/:id", (req, res) => {
+  var templateVars = { shortURL: req.params.id,
+   urls: urlDatabase};
+  res.render("urls_show", templateVars);
+  //res.render("urls_show", {agent: req.headers["user-agent"]});
+});
+
+app.get("/urls", (req, res) => {
+  var templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {

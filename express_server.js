aa9e16265db2d6e48ac97e9981d8bbec9d078e86
michaelrychly <MichaelRychly@gmail.com>
2018-03-21 16:13:39 -0400

    added comments

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 "use strict";
+//parameters
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -6,6 +7,7 @@ const cookieParser = require("cookie-parser");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+//connecting to express
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -15,11 +17,13 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//http get methods
 app.get("/", (req, res) => {
   res.render("Hello!");
 });
 
 app.get("/urls/new", (req, res) => {
+  //saving the reuired variables
   let templateVars = {
     urls: urlDatabase,
     username: req.cookies["username"]
@@ -29,71 +33,102 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = "";
+
+  //search for the url in url database
   for (let url in urlDatabase){
     if (req.params.shortURL === url){
       longURL = urlDatabase[url];
     }
   }
+
+  //redirect to the long URL
   res.redirect(301, longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
+  //saving the reuired variables
   let templateVars = {
     shortURL: req.params.id,
     urls: urlDatabase,
     username: req.cookies["username"]
   };
+
+  //redirect to the short url details
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => {
+  //saving the reuired variables
   let templateVars = {
     urls: urlDatabase,
     username: req.cookies["username"]
   };
+
+  //redirect to the url overview list
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(301, '/urls/' + shortURL);
-});
-
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect(301, '/urls');
-});
-
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect(301, '/urls');
-});
-
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect(301, '/urls');
-});
-
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect(301, '/urls');
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect(301, '/urls');
-});
-
 app.get("/urls.json", (req, res) => {
+  //return the url database as json
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
+  //return hello world as html string
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//http post methods
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+
+  urlDatabase[shortURL] = req.body.longURL;
+
+  //redirect to the short url details
+  res.redirect(301, '/urls/' + shortURL);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  //delete the url from the url database
+  delete urlDatabase[req.params.id];
+
+  //redirect to the url overview list
+  res.redirect(301, '/urls');
+});
+
+app.post("/urls/:id", (req, res) => {
+  //add the url to the url database
+  urlDatabase[req.params.id] = req.body.longURL;
+
+  //redirect to url overview list
+  res.redirect(301, '/urls');
+});
+
+app.post("/urls/:id", (req, res) => {
+  //update the long url for a particular short url
+  urlDatabase[req.params.id] = req.body.longURL;
+
+  //redirect to url overview list
+  res.redirect(301, '/urls');
+});
+
+app.post("/login", (req, res) => {
+  //create the cookie username and save the provided username
+  res.cookie('username', req.body.username);
+
+  //redirect to the url overview list
+  res.redirect(301, '/urls');
+});
+
+app.post("/logout", (req, res) => {
+  //delete the cookie username
+  res.clearCookie('username');
+
+  //redirect to the url overview list
+  res.redirect(301, '/urls');
+});
+
+//http listener
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -101,6 +136,8 @@ app.listen(PORT, () => {
 function generateRandomString() {
   let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let result = '';
+
+  //create a random string that has 6 alphanumeric charactars
   for (var i = 6; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 }
